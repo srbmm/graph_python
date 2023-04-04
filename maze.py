3a28fc7f19a155d370790f787716b7b64ab5990b
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-04-04 20:26:58 +0330

    plot added

--- maze.py
+++ maze.py
@@ -1,21 +1,21 @@
 from Graph import Graph
 import matplotlib.pyplot as plt
 
-# inp ex1
-# inp = """0 0 1 1 0
-# 0 0 0 1 0
-# 0 1 0 0 0
-# 0 1 1 0 0
-# 1 0 1 1 0
-# 0 1 0 0 0
-# 0 0 0 1 1
-# 0 1 0 1 1"""
+#inp ex1
+inp = """0 0 1 1 0
+0 0 0 1 0
+0 1 0 0 0
+0 1 1 0 0
+1 0 1 1 0
+0 1 0 0 0
+0 0 0 1 1
+0 1 0 1 1"""
 
-inp = ""
-for i in range(8):
-    inp += input(f"line-{i + 1}:")
-    if i != 7:
-        inp += "\n"
+# inp = ""
+# for i in range(8):
+#     inp += input(f"line-{i + 1}:")
+#     if i != 7:
+#         inp += "\n"
 
 plt.axis([1, 6, 6, 1])
 
@@ -57,10 +57,15 @@ for i in range(5):
 
 
 res = my_graph.ucs('1,1', '5,5')
-for i in res[1]:
-    i, j = i.split(",")
-    i = int(i)
-    j = int(j)
-    plt.scatter(j+0.5, i+0.5)
+for i in range(len(res[1]) - 1):
+    temp_i, temp_j = res[1][i].split(",")
+    temp_i = int(temp_i)
+    temp_j = int(temp_j)
+    next_i, next_j = res[1][i+1].split(",")
+    next_i = int(next_i)
+    next_j = int(next_j)
+    print([temp_j+0.5, next_j+0.5], [temp_i+0.5, next_i+0.5])
+    plt.plot([temp_j+0.5, next_j+0.5], [temp_i+0.5, next_i+0.5], "-o", linewidth=2)
+
 plt.show()
 print(res)
